c8fdbcb4aa259813f990c0d12ab0adcadffa0cbf
Acreed21 <cgcontrerasm@unal.edu.co>
2021-10-31 03:03:11 -0500

    Taller 2

--- Taller_2 (2 de noviembre)/app/src/main/java/com/example/triqui/MainActivity.kt
+++ Taller_2 (2 de noviembre)/app/src/main/java/com/example/triqui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.triqui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.TextView
@@ -18,22 +19,14 @@ class MainActivity : AppCompatActivity() {
     private val six by lazy<TextView> { findViewById(R.id.six) }
     private val seven by lazy<TextView> { findViewById(R.id.seven) }
     private val eight by lazy<TextView> { findViewById(R.id.eight) }
-    private var zeroState = false
-    private var oneState = false
-    private var twoState = false
-    private var threeState = false
-    private var fourState = false
-    private var fiveState = false
-    private var sixState = false
-    private var sevenState = false
-    private var eightState = false
+    private val reset by lazy<TextView> { findViewById(R.id.reset) }
     private var turn = false
-    private var numero = (random() * 10 + 1).toInt()
-    private val mBoard = charArrayOf('1', '2', '3', '4', '5', '6', '7', '8', '9')
+    private var mBoard = charArrayOf('1', '2', '3', '4', '5', '6', '7', '8', '9')
     val HUMAN_PLAYER = 'X'
     val COMPUTER_PLAYER = 'O'
     private var BOARD_SIZE = 9
     private var finish = false
+    //private val mRand: Random? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,216 +35,254 @@ class MainActivity : AppCompatActivity() {
         setListeners(animationScale)
     }
 
-    // Check for a winner.  Return
-    //  0 if no winner or tie yet
-    //  1 if it's a tie
-    //  2 if X won
-    //  3 if O won
-
-
-    private fun checkForWinner() {
+    private fun checkForWinner(): Int {
         run {
             var i = 0
             while (i <= 6) {
-                if (mBoard[i] === HUMAN_PLAYER && mBoard[i + 1] === HUMAN_PLAYER && mBoard[i + 2] === HUMAN_PLAYER
+                if (mBoard[i] == HUMAN_PLAYER && mBoard[i + 1] == HUMAN_PLAYER && mBoard[i + 2] == HUMAN_PLAYER
                 ) {
                     Toast.makeText(this, "Ganó el Humano", Toast.LENGTH_LONG).show()
                     finish = true
+                    return 2
                 }
-                if (mBoard[i] === COMPUTER_PLAYER && mBoard[i + 1] === COMPUTER_PLAYER && mBoard[i + 2] === COMPUTER_PLAYER
+                if (mBoard[i] == COMPUTER_PLAYER && mBoard[i + 1] == COMPUTER_PLAYER && mBoard[i + 2] == COMPUTER_PLAYER
                 ) {
                     Toast.makeText(this, "Ganó la máquina", Toast.LENGTH_LONG).show()
                     finish = true
+                    return 3
                 }
                 i += 3
             }
         }
         for (i in 0..2) {
-            if (mBoard[i] === HUMAN_PLAYER && mBoard[i + 3] === HUMAN_PLAYER && mBoard[i + 6] === HUMAN_PLAYER) {
+            if (mBoard[i] == HUMAN_PLAYER && mBoard[i + 3] == HUMAN_PLAYER && mBoard[i + 6] == HUMAN_PLAYER) {
                 Toast.makeText(
                     this,
                     "Ganó el Humano",
                     Toast.LENGTH_LONG
                 ).show()
                 finish = true
+                return 2
             }
-            if (mBoard[i] === COMPUTER_PLAYER && mBoard[i + 3] === COMPUTER_PLAYER && mBoard[i + 6] === COMPUTER_PLAYER) {
+            if (mBoard[i] == COMPUTER_PLAYER && mBoard[i + 3] == COMPUTER_PLAYER && mBoard[i + 6] == COMPUTER_PLAYER) {
                 Toast.makeText(
                     this,
                     "Ganó la máquina",
                     Toast.LENGTH_LONG
                 ).show()
                 finish = true
+                return 3
             }
         }
-        if (mBoard[0] === HUMAN_PLAYER && mBoard[4] === HUMAN_PLAYER && mBoard[8] === HUMAN_PLAYER ||
-            mBoard[2] === HUMAN_PLAYER && mBoard[4] === HUMAN_PLAYER && mBoard[6] === HUMAN_PLAYER
+        if (mBoard[0] == HUMAN_PLAYER && mBoard[4] == HUMAN_PLAYER && mBoard[8] == HUMAN_PLAYER ||
+            mBoard[2] == HUMAN_PLAYER && mBoard[4] == HUMAN_PLAYER && mBoard[6] == HUMAN_PLAYER
         ) {
             Toast.makeText(this, "Ganó el Humano", Toast.LENGTH_LONG).show()
-            finish=true
+            finish = true
+            return 2
         }
-        if (mBoard[0] === COMPUTER_PLAYER && mBoard[4] === COMPUTER_PLAYER && mBoard[8] === COMPUTER_PLAYER ||
-            mBoard[2] === COMPUTER_PLAYER && mBoard[4] === COMPUTER_PLAYER && mBoard[6] === COMPUTER_PLAYER
+        if (mBoard[0] == COMPUTER_PLAYER && mBoard[4] == COMPUTER_PLAYER && mBoard[8] == COMPUTER_PLAYER ||
+            mBoard[2] == COMPUTER_PLAYER && mBoard[4] == COMPUTER_PLAYER && mBoard[6] == COMPUTER_PLAYER
         ) {
             Toast.makeText(this, "Ganó la máquina", Toast.LENGTH_LONG).show()
-            finish=true
+            finish = true
+            return 3
         }
-        for (i in 0 until BOARD_SIZE) {
-            if (mBoard[i] !== HUMAN_PLAYER && mBoard[i] !== COMPUTER_PLAYER) {
+        if (BOARD_SIZE == 0) {
+            Toast.makeText(this, "Empate", Toast.LENGTH_LONG).show()
+            finish = true
+            return 1
+        }
+        return 0
+    }
 
+    private fun checkForWinnerSimulacion(): Int {
+        run {
+            var i = 0
+            while (i <= 6) {
+                if (mBoard[i] == HUMAN_PLAYER && mBoard[i + 1] == HUMAN_PLAYER && mBoard[i + 2] == HUMAN_PLAYER
+                ) {
+                    return 2
+                }
+                if (mBoard[i] == COMPUTER_PLAYER && mBoard[i + 1] == COMPUTER_PLAYER && mBoard[i + 2] == COMPUTER_PLAYER
+                ) {
+                    return 3
+                }
+                i += 3
             }
         }
-        //Toast.makeText(this, "Empate", Toast.LENGTH_LONG).show()
+        for (i in 0..2) {
+            if (mBoard[i] == HUMAN_PLAYER && mBoard[i + 3] == HUMAN_PLAYER && mBoard[i + 6] == HUMAN_PLAYER) {
+                return 2
+            }
+            if (mBoard[i] == COMPUTER_PLAYER && mBoard[i + 3] == COMPUTER_PLAYER && mBoard[i + 6] == COMPUTER_PLAYER) {
+
+                return 3
+            }
+        }
+        if (mBoard[0] == HUMAN_PLAYER && mBoard[4] == HUMAN_PLAYER && mBoard[8] == HUMAN_PLAYER ||
+            mBoard[2] == HUMAN_PLAYER && mBoard[4] == HUMAN_PLAYER && mBoard[6] == HUMAN_PLAYER
+        ) {
+            return 2
+        }
+        if (mBoard[0] == COMPUTER_PLAYER && mBoard[4] == COMPUTER_PLAYER && mBoard[8] == COMPUTER_PLAYER ||
+            mBoard[2] == COMPUTER_PLAYER && mBoard[4] == COMPUTER_PLAYER && mBoard[6] == COMPUTER_PLAYER
+        ) {
+            return 3
+        }
+        if (BOARD_SIZE == 0) {
+            return 1
+        }
+        return 0
     }
 
     private fun setListeners(animationScale: Animation) {
         zero.setOnClickListener {
-            if (!zeroState && !finish) {
-                it.startAnimation(animationScale)
-                if (!turn) {
-                    zero.background = resources.getDrawable(R.drawable.x)
-                    mBoard[0] = HUMAN_PLAYER
-                } else {
-                    zero.background = resources.getDrawable(R.drawable.o)
-                    mBoard[0] = COMPUTER_PLAYER
-                }
-                zeroState = true
-                turn = !turn
-                BOARD_SIZE--
-                checkForWinner()
+            if (mBoard[0] == '1') {
+                callFunction(animationScale, it, zero, 0)
+                getComputerMove()
             }
         }
         one.setOnClickListener {
-            if (!oneState && !finish) {
-                it.startAnimation(animationScale)
-                if (!turn) {
-                    one.background = resources.getDrawable(R.drawable.x)
-                    mBoard[1] = HUMAN_PLAYER
-                } else {
-                    one.background = resources.getDrawable(R.drawable.o)
-                    mBoard[1] = COMPUTER_PLAYER
-                }
-                oneState = true
-                turn = !turn
-                BOARD_SIZE--
-                checkForWinner()
+            if (mBoard[1] == '2') {
+                callFunction(animationScale, it, one, 1)
+                getComputerMove()
             }
         }
         two.setOnClickListener {
-            if (!twoState && !finish) {
-                it.startAnimation(animationScale)
-                if (!turn) {
-                    two.background = resources.getDrawable(R.drawable.x)
-                    mBoard[2] = HUMAN_PLAYER
-                } else {
-                    two.background = resources.getDrawable(R.drawable.o)
-                    mBoard[2] = COMPUTER_PLAYER
-                }
-                twoState = true
-                turn = !turn
-                BOARD_SIZE--
-                checkForWinner()
+            if (mBoard[2] == '3') {
+                callFunction(animationScale, it, two, 2)
+                getComputerMove()
             }
         }
         three.setOnClickListener {
-            if (!threeState && !finish) {
-                it.startAnimation(animationScale)
-                if (!turn) {
-                    three.background = resources.getDrawable(R.drawable.x)
-                    mBoard[3] = HUMAN_PLAYER
-                } else {
-                    three.background = resources.getDrawable(R.drawable.o)
-                    mBoard[3] = COMPUTER_PLAYER
-                }
-                threeState = true
-                turn = !turn
-                BOARD_SIZE--
-                checkForWinner()
+            if (mBoard[3] == '4') {
+                callFunction(animationScale, it, three, 3)
+                getComputerMove()
             }
         }
         four.setOnClickListener {
-            if (!fourState && !finish) {
-                it.startAnimation(animationScale)
-                if (!turn) {
-                    four.background = resources.getDrawable(R.drawable.x)
-                    mBoard[4] = HUMAN_PLAYER
-                } else {
-                    four.background = resources.getDrawable(R.drawable.o)
-                    mBoard[4] = COMPUTER_PLAYER
-                }
-                fourState = true
-                turn = !turn
-                BOARD_SIZE--
-                checkForWinner()
+            if (mBoard[4] == '5') {
+                callFunction(animationScale, it, four, 4)
+                getComputerMove()
             }
         }
         five.setOnClickListener {
-            if (!fiveState && !finish) {
-                it.startAnimation(animationScale)
-                if (!turn) {
-                    five.background = resources.getDrawable(R.drawable.x)
-                    mBoard[5] = HUMAN_PLAYER
-                } else {
-                    five.background = resources.getDrawable(R.drawable.o)
-                    mBoard[5] = COMPUTER_PLAYER
-                }
-                fiveState = true
-                turn = !turn
-                BOARD_SIZE--
-                checkForWinner()
+            if (mBoard[5] == '6') {
+                callFunction(animationScale, it, five, 5)
+                getComputerMove()
             }
         }
         six.setOnClickListener {
-            if (!sixState && !finish) {
-                it.startAnimation(animationScale)
-                if (!turn) {
-                    six.background = resources.getDrawable(R.drawable.x)
-                    mBoard[6] = HUMAN_PLAYER
-                } else {
-                    six.background = resources.getDrawable(R.drawable.o)
-                    mBoard[6] = COMPUTER_PLAYER
-                }
-                sixState = true
-                turn = !turn
-                BOARD_SIZE--
-                checkForWinner()
+            if (mBoard[6] == '7') {
+                callFunction(animationScale, it, six, 6)
+                getComputerMove()
             }
         }
         seven.setOnClickListener {
-            if (!sevenState && !finish) {
-                it.startAnimation(animationScale)
-                if (!turn) {
-                    seven.background = resources.getDrawable(R.drawable.x)
-                    mBoard[7] = HUMAN_PLAYER
-                } else {
-                    seven.background = resources.getDrawable(R.drawable.o)
-                    mBoard[7] = COMPUTER_PLAYER
-                }
-                sevenState = true
-                turn = !turn
-                BOARD_SIZE--
-                checkForWinner()
+            if (mBoard[7] == '8') {
+                callFunction(animationScale, it, seven, 7)
+                getComputerMove()
             }
         }
         eight.setOnClickListener {
-            if (!eightState && !finish) {
-                it.startAnimation(animationScale)
-                if (!turn) {
-                    eight.background = resources.getDrawable(R.drawable.x)
-                    mBoard[7] = HUMAN_PLAYER
-                } else {
-                    eight.background = resources.getDrawable(R.drawable.o)
-                    mBoard[7] = COMPUTER_PLAYER
-                }
-                eightState = true
-                turn = !turn
-                BOARD_SIZE--
-                checkForWinner()
+            if (mBoard[8] == '9') {
+                callFunction(animationScale, it, eight, 8)
+                getComputerMove()
             }
+        }
+        reset.setOnClickListener {
+            mBoard = charArrayOf('1', '2', '3', '4', '5', '6', '7', '8', '9')
+            zero.background = resources.getDrawable(R.drawable.vacio)
+            one.background = resources.getDrawable(R.drawable.vacio)
+            two.background = resources.getDrawable(R.drawable.vacio)
+            three.background = resources.getDrawable(R.drawable.vacio)
+            four.background = resources.getDrawable(R.drawable.vacio)
+            five.background = resources.getDrawable(R.drawable.vacio)
+            six.background = resources.getDrawable(R.drawable.vacio)
+            seven.background = resources.getDrawable(R.drawable.vacio)
+            eight.background = resources.getDrawable(R.drawable.vacio)
+            BOARD_SIZE = 9
+            finish = false
         }
     }
 
-    private fun press() {
+    private fun callFunction(
+        animationScale: Animation,
+        view: View,
+        text: View,
+        i: Int
+    ): Boolean {
+        if (!finish && (mBoard[i] != HUMAN_PLAYER || mBoard[i] != COMPUTER_PLAYER)) {
+            view.startAnimation(animationScale)
+            if (!turn) {
+                text.background = resources.getDrawable(R.drawable.x)
+                mBoard[i] = HUMAN_PLAYER
+            } else {
+                text.background = resources.getDrawable(R.drawable.o)
+                mBoard[i] = COMPUTER_PLAYER
+            }
+            turn = !turn
+            BOARD_SIZE--
+            checkForWinner()
+            return true
+        } else {
+            return false
+        }
+    }
 
+    private fun validarTextView(i: Int): TextView {
+        when (i) {
+            0 -> return zero
+            1 -> return one
+            2 -> return two
+            3 -> return three
+            4 -> return four
+            5 -> return five
+            6 -> return six
+            7 -> return seven
+            8 -> return eight
+            else -> return zero
+        }
+    }
+
+    private fun getComputerMove() {
+        if (BOARD_SIZE != 0) {
+            var move: Int
+            val animationScale = AnimationUtils.loadAnimation(this, R.anim.button_choice_small)
+            // First see if there's a move O can make to win
+            for (i in 0..8) {
+                if (mBoard[i] != HUMAN_PLAYER && mBoard[i] != COMPUTER_PLAYER) {
+                    val curr = mBoard[i]
+                    mBoard[i] = COMPUTER_PLAYER
+                    if (checkForWinnerSimulacion() == 3) {
+                        var textView = validarTextView(i)
+                        callFunction(animationScale, textView, textView, i)
+                        return
+                    } else mBoard[i] = curr
+                }
+            }
+
+            // See if there's a move O can make to block X from winning
+            for (i in 0..8) {
+                if (mBoard[i] != HUMAN_PLAYER && mBoard[i] != COMPUTER_PLAYER) {
+                    val curr = mBoard[i] // Save the current number
+                    mBoard[i] = HUMAN_PLAYER
+                    if (checkForWinnerSimulacion() == 2) {
+                        var textView = validarTextView(i)
+                        callFunction(animationScale, textView, textView, i)
+                        return
+                    } else mBoard[i] = curr
+                }
+            }
+
+            // Generate random move
+            do {
+                var mRand = (random() * 9).toInt()
+                move = mRand //.nextInt(BOARD_SIZE)
+            } while (mBoard[move] == HUMAN_PLAYER || mBoard[move] == COMPUTER_PLAYER)
+            var textView = validarTextView(move)
+            callFunction(animationScale, textView, textView, move)
+        }
     }
 }
